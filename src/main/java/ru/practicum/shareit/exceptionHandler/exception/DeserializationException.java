5834d2dddbd2b0a26c0cbfdf9877831be18152f8
Kudrin <alekseykudrin91@yandex.ru>
2023-01-30 22:46:02 +0300

    Fix

--- src/main/java/ru/practicum/shareit/exceptionHandler/exception/DeserializationException.java
+++
@@ -1,9 +0,0 @@
-package ru.practicum.shareit.exceptionHandler.exception;
-
-import org.springframework.web.bind.MethodArgumentNotValidException;
-
-public class DeserializationException extends MethodArgumentNotValidException {
-    public DeserializationException(String message) {
-        super(null, null);
-    }
-}
