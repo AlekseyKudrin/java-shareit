d9beca2b552a6403e9142cb672630f77841a9616
frade <AlekseyKudrin91@yandex.ru>
2023-05-08 10:38:20 +0300

    fix

--- gateway/src/main/java/ru/practicum/shareit/exceptionHandler/ErrorHandler.java
+++ gateway/src/main/java/ru/practicum/shareit/exceptionHandler/ErrorHandler.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import ru.practicum.shareit.exceptionHandler.exception.ValidationTimeException;
 
 @Slf4j
 @RestControllerAdvice
@@ -28,6 +29,13 @@ public class ErrorHandler {
     @ExceptionHandler
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse handlerMethodArgumentNotValidException(final MethodArgumentNotValidException e) {
+        log.error("Data input incorrect: {}", e.getMessage());
+        return new ErrorResponse(e.getMessage());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handlerValidationTimeException(final ValidationTimeException e) {
         log.error("Data input incorrect: {}", e.getMessage());
         return new ErrorResponse(e.getMessage());
     }
